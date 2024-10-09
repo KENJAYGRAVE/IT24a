8763cba62f1c1f699b37ddc1a8bac04dc8eae599
kenjay <kenjaygrave20v2@gmail.com>
2024-10-09 16:34:10 +0800

    added filterStudents

--- applet-4.js
+++ applet-4.js
@@ -36,5 +36,15 @@ bindSearchEvent() {
 
     this.renderStudentList(this.students, studentSearchListContainer);
 }
+filterStudents(query, searchListContainer) {
+    const filteredStudents = this.students.filter(student => {
+        const fullName = `${student.student_name} ${student.student_program}`;
+        return fullName.toLowerCase().includes(query.toLowerCase());
+    });
+
+    searchListContainer.innerHTML = '';
+
+    this.renderStudentList(filteredStudents, searchListContainer);
+}
 
 }
